fa9eea4cb53afb434e40df042e0e0801f62a77cb
John Hawkins <john.hawkins@revelry.co>
2019-04-24 11:03:23 -0700

    Add rev wrapper to 404 page

--- 404.php
+++ 404.php
@@ -10,21 +10,23 @@
 get_header(); ?>
 
 	<main id="main" class="site-main">
+		<div class="rev-ContentWrapper">
+			<div class="rev-Content">
+				<section class="error-404 not-found container">
+					<header class="page-header">
+						<h1 class="page-title"><?php esc_html_e( 'Sorry, this page doesn\'t exist.', 'harmonium' ); ?></h1>
+					</header><!-- .page-header -->
 
-		<section class="error-404 not-found container">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Sorry, this page doesn\'t exist.', 'harmonium' ); ?></h1>
-			</header><!-- .page-header -->
+					<div class="page-content">
 
-			<div class="page-content">
+						<p><?php esc_html_e( 'It seems we can\'t find what you\'re looking for. Perhaps searching can help.', 'harmonium' ); ?></p>
 
-				<p><?php esc_html_e( 'It seems we can\'t find what you\'re looking for. Perhaps searching can help.', 'harmonium' ); ?></p>
+						<?php get_search_form(); ?>
 
-				<?php get_search_form(); ?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-
+					</div><!-- .page-content -->
+				</section><!-- .error-404 -->
+			</div><!-- end rev-Content -->
+		</div><!-- end rev-ContentWrapper -->
 	</main><!-- #main -->
 
 <?php get_footer(); ?>
