9ee027bf40a014c84f84ced22e9a82ce218e3593
John Hawkins <john.hawkins@revelry.co>
2019-04-24 10:47:31 -0700

    Add rev wrappers to search

--- search.php
+++ search.php
@@ -10,36 +10,47 @@
 get_header(); ?>
 
 	<main id="main" class="site-main">
+		<div class="rev-ContentWrapper">
+			<div class="rev-Content">
+				<div class="rev-Row">
+					<div class="rev-Col rev-Col--small9">
 
-		<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<h1 class="page-title"><?php /* translators: the term(s) searched */ printf( esc_html__( 'Search Results for: %s', 'harmonium' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
+						<?php if ( have_posts() ) : ?>
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+							<header class="page-header">
+								<h1 class="page-title"><?php /* translators: the term(s) searched */ printf( esc_html__( 'Search Results for: %s', 'harmonium' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+							</header><!-- .page-header -->
 
-				/**
-					* Run the loop for the search to output the results.
-					* If you want to overload this in a child theme then include a file
-					* called content-search.php and that will be used instead.
-					*/
-				get_template_part( 'template-parts/content', 'search' );
+							<?php
+							/* Start the Loop */
+							while ( have_posts() ) :
+								the_post();
 
-			endwhile;
+								/**
+									* Run the loop for the search to output the results.
+									* If you want to overload this in a child theme then include a file
+									* called content-search.php and that will be used instead.
+									*/
+								get_template_part( 'template-parts/content', 'search' );
 
-			harmonium_display_numeric_pagination();
+							endwhile;
 
-		else :
+							harmonium_display_numeric_pagination();
 
-			get_template_part( 'template-parts/content', 'none' );
+						else :
 
-		endif;
-		?>
+							get_template_part( 'template-parts/content', 'none' );
 
+						endif;
+						?>
+					</div><!-- rev-Col -->
+					<div class="rev-Col rev-Col--small3">
+						<?php get_sidebar(); ?>
+					</div><!-- rev-Col -->
+				</div><!-- rev-Row -->
+			</div><!-- rev-Content -->
+		</div><!-- rev-ContentWrapper -->
 	</main><!-- #main -->
 
 <?php get_footer(); ?>
