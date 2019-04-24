429cf3add64042e43db2836adbcbdfb79b8cf02b
John Hawkins <john.hawkins@revelry.co>
2019-04-24 10:40:36 -0700

    Add rev wrappers to Single

--- single.php
+++ single.php
@@ -11,23 +11,27 @@ get_header(); ?>
 
 	<div class="display-flex grid-wrapper">
 		<main id="main" class="site-main">
+			<div class="rev-ContentWrapper">
+				<div class="rev-Content">
 
-			<?php
-			while ( have_posts() ) :
-				the_post();
 
-				get_template_part( 'template-parts/content', get_post_format() );
+					<?php
+					while ( have_posts() ) :
+						the_post();
 
-				the_post_navigation();
+						get_template_part( 'template-parts/content', get_post_format() );
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+						the_post_navigation();
 
-			endwhile; // End of the loop.
-			?>
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;
 
+					endwhile; // End of the loop.
+					?>
+				</div><!-- end rev-Content -->
+			<!-- end rev-ContentWrapper -->
 		</main><!-- #main -->
 	</div><!-- .grid-wrapper -->
 <?php get_footer(); ?>
