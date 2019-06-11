97ab61e0bfbcc1efadc7f80d08b86268dc1a5c30
John Hawkins <john.hawkins@revelry.co>
2019-06-11 10:14:49 -0700

    Add rev elements to text-media block

--- template-parts/content-blocks/block-fifty_text_media.php
+++ template-parts/content-blocks/block-fifty_text_media.php
@@ -13,19 +13,20 @@ $text       = get_sub_field( 'text_primary' );
 harmonium_display_block_options(
 	array(
 		'container' => 'section', // Any HTML5 container: section, div, etc...
-		'class'     => 'content-block grid-container fifty-fifty fifty-text-media', // Container class.
+		'class'     => 'rev-home-hero rev-Row'
+		// 'class'     => 'content-block grid-container fifty-fifty fifty-text-media', // Container class.
 	)
 );
 ?>
-	<div class="display-flex container<?php echo esc_attr( harmonium_get_animation_class() ); ?>">
+	<div class="rev-Row rev-Row--flex rev-Row--justifyCenter display-flex container<?php echo esc_attr( harmonium_get_animation_class() ); ?>">
+		<div class="rev-Row">
+			<div class="rev-Col rev-Col--medium6">
+				<?php echo harmonium_get_the_content( $text ); // WPCS: XSS OK. ?>
+			</div>
 
-		<div class="half">
-			<?php echo harmonium_get_the_content( $text ); // WPCS: XSS OK. ?>
-		</div>
-
-		<div class="half">
-			<img class="fifty-media-image" src="<?php echo esc_url( $image_data['url'] ); ?>" alt="<?php echo esc_html( $image_data['alt'] ); ?>">
-		</div>
-
+			<div class="rev-Col rev-Col--medium6">
+				<img class="fifty-media-image" src="<?php echo esc_url( $image_data['url'] ); ?>" alt="<?php echo esc_html( $image_data['alt'] ); ?>">
+			</div>
+		</div><!-- .rev-Row -->
 	</div><!-- .container -->
 </section><!-- .fifty-text-media -->
