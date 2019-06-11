9ffb8c26fac9eedd1d6bf7dc12afdcb17e127066
John Hawkins <john.hawkins@revelry.co>
2019-06-11 11:45:20 -0700

    add rev elements to CTA block

--- template-parts/content-blocks/block-cta.php
+++ template-parts/content-blocks/block-cta.php
@@ -13,28 +13,32 @@ $text        = get_sub_field( 'text' );
 harmonium_display_block_options(
 	array(
 		'container' => 'aside', // Any HTML5 container: section, div, etc...
-		'class'     => 'content-block cta-block', // Container class.
+		'class'     => 'rev-home-hero rev-Row' // Container class.
 	)
 );
 ?>
-	<div class="container display-flex align-center<?php echo esc_attr( harmonium_get_animation_class() ); ?>">
-		<header>
-			<?php if ( $block_title ) : ?>
-				<h1 class="cta-title"><?php echo esc_html( $block_title ); ?></h1>
-			<?php endif; ?>
+	<div class="container rev-Row rev-Row--flex rev-Row--justifyCenter <?php echo esc_attr( harmonium_get_animation_class() ); ?>">
+		<div class="rev-Row rev-Row--flex">
+			<div class="rev-Col rev-Col--medium6 rev-Col--small12">
+				<?php if ( $block_title ) : ?>
+					<h1 class="cta-title"><?php echo esc_html( $block_title ); ?></h1>
+				<?php endif; ?>
 
-			<?php if ( $text ) : ?>
-				<h2 class="cta-text"><?php echo esc_html( $text ); ?></h2>
-			<?php endif; ?>
-		</header>
-
-		<?php
-		harmonium_display_link(
-			array(
-				'button' => true,
-				'class'  => 'button-cta',
-			)
-		);
-		?>
+				<?php if ( $text ) : ?>
+					<h2 class="cta-text"><?php echo esc_html( $text ); ?></h2>
+				<?php endif; ?>
+			</div><!-- .rev-Col -->
+		
+			<div class="rev-Col rev-Col--medium6 rev-Col--small12">
+				<?php
+				harmonium_display_link(
+					array(
+						'button' => true,
+						'class'  => 'button-cta',
+					)
+				);
+				?>
+			</div><!-- .rev-Col -->
+		</div><!-- .rev-Row -->
 	</div><!-- .container -->
 </aside><!-- .cta-block -->
