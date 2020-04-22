7c2703066f90ab4e1ea36d7328efe12ec75517d1
John Hawkins <john.hawkins@revelry.co>
2020-04-22 09:44:07 -0700

    wrapped two more blocks in rev syntax

--- template-parts/content-blocks/block-wds-recent-posts.php
+++ template-parts/content-blocks/block-wds-recent-posts.php
@@ -39,43 +39,48 @@ if ( $recent_posts->have_posts() ) :
 		array(
 			'block'     => $block,
 			'container' => 'section', // Any HTML5 container: section, div, etc...
-			'class'     => 'content-block recent-posts-block' . esc_attr( $alignment . $classes ), // Container class.
+			'class'     => 'rev-Row rev-Row--flex rev-Row--justifyCenter content-block recent-posts-block' . esc_attr( $alignment . $classes ), // Container class.
 		)
 	);
 	?>
 
-		<div class="container">
-			<?php if ( $block_title ) : ?>
-			<h2 class="content-block-title"><?php echo esc_html( $block_title ); ?></h2>
-			<?php endif; ?>
-		</div>
+		<div class="rev-Row">
+			<div class="rev-Col">
 
-		<div class="container display-flex">
+				<div class="container">
+					<?php if ( $block_title ) : ?>
+					<h2 class="content-block-title"><?php echo esc_html( $block_title ); ?></h2>
+					<?php endif; ?>
+				</div>
 
-			<?php
-			// Loop through recent posts.
-			while ( $recent_posts->have_posts() ) :
-				$recent_posts->the_post();
+				<div class="rev-Row rev-Row--flex container display-flex">
 
-				// Display a card.
-				harmonium_display_card(
-					array(
-						'title' => get_the_title(),
-						'image' => wp_get_attachment_image( get_post_thumbnail_id(), 'medium', false, array( 'card-image' ) ),
-						'text'  => harmonium_get_the_excerpt(
+					<?php
+					// Loop through recent posts.
+					while ( $recent_posts->have_posts() ) :
+						$recent_posts->the_post();
+
+						// Display a card.
+						harmonium_display_card(
 							array(
-								'length' => 20,
-								'more'   => '...',
+								'title' => get_the_title(),
+								'image' => wp_get_attachment_image( get_post_thumbnail_id(), 'medium', false, array( 'card-image' ) ),
+								'text'  => harmonium_get_the_excerpt(
+									array(
+										'length' => 20,
+										'more'   => '...',
+									)
+								),
+								'url'   => get_the_permalink(),
+								'class' => 'rev-Col rev-Col--medium4 rev-Col--small12',
 							)
-						),
-						'url'   => get_the_permalink(),
-						'class' => 'third',
-					)
-				);
+						);
 
-			endwhile;
-			wp_reset_postdata();
-			?>
-		</div>
+					endwhile;
+					wp_reset_postdata();
+					?>
+				</div>
+			</div><!-- end rev-Col -->
+		</div><!-- end rev-Row -->
 	</section>
 <?php endif; ?>
--- template-parts/content-blocks/block-wds-related-posts.php
+++ template-parts/content-blocks/block-wds-related-posts.php
@@ -19,47 +19,51 @@ if ( $related_posts ) :
 		array(
 			'block'     => $block,
 			'container' => 'section', // Any HTML5 container: section, div, etc...
-			'class'     => 'content-block related-posts-block' . esc_attr( $alignment . $classes ), // Container class.
+			'class'     => 'rev-Row rev-Row--flex rev-Row--justifyCenter content-block related-posts-block' . esc_attr( $alignment . $classes ), // Container class.
 		)
 	);
 	?>
 
-		<div class="container">
-			<?php if ( $block_title ) : ?>
-				<h2 class="content-block-title"><?php echo esc_html( $block_title ); ?></h2>
-			<?php endif; ?>
-		</div>
+		<div class="rev-Row">
+			<div class="rev-Col">
+				<div class="container">
+					<?php if ( $block_title ) : ?>
+						<h2 class="content-block-title"><?php echo esc_html( $block_title ); ?></h2>
+					<?php endif; ?>
+				</div>
 
-		<div class="container display-flex">
+				<div class="container display-flex">
 
-			<?php
-			// Loop through recent posts.
-			foreach ( $related_posts as $key => $post ) :// @codingStandardsIgnoreLine
+					<?php
+					// Loop through recent posts.
+					foreach ( $related_posts as $key => $post ) :// @codingStandardsIgnoreLine
 
-				// Convert post object to post data.
-				setup_postdata( $post );
+						// Convert post object to post data.
+						setup_postdata( $post );
 
-				// Display a card.
-				harmonium_display_card(
-					array(
-						'title' => get_the_title( $post ),
-						'image' => wp_get_attachment_image( get_post_thumbnail_id( $post->ID ), 'medium', false, array( 'card-image' ) ),
-						'text'  => harmonium_get_the_excerpt(
+						// Display a card.
+						harmonium_display_card(
 							array(
-								'length' => 20,
-								'more'   => '...',
-								'post'   => $post,
+								'title' => get_the_title( $post ),
+								'image' => wp_get_attachment_image( get_post_thumbnail_id( $post->ID ), 'medium', false, array( 'card-image' ) ),
+								'text'  => harmonium_get_the_excerpt(
+									array(
+										'length' => 20,
+										'more'   => '...',
+										'post'   => $post,
+									)
+								),
+								'url'   => get_the_permalink( $post ),
+								'class' => 'third',
 							)
-						),
-						'url'   => get_the_permalink( $post ),
-						'class' => 'third',
-					)
-				);
+						);
 
-			endforeach;
-			wp_reset_postdata();
-		?>
-		</div>
+					endforeach;
+					wp_reset_postdata();
+				?>
+				</div>
+			</div><!-- end rev-Col -->
+		</div><!-- end rev-Row -->
 	</section>
 <?php elseif ( ! $related_posts && is_admin() ) : ?>
 	<?php
@@ -68,7 +72,7 @@ if ( $related_posts ) :
 		array(
 			'block'     => $block,
 			'container' => 'section', // Any HTML5 container: section, div, etc...
-			'class'     => 'content-block related-posts-block' . esc_attr( $alignment . $classes ), // Container class.
+			'class'     => 'rev-Row rev-Row--flex content-block related-posts-block' . esc_attr( $alignment . $classes ), // Container class.
 		)
 	);
 	?>
